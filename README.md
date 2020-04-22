94e4a8c2e212c6d4b68ee0adaaf9716fa415af1c
Lakshmeesha <lakshmeesha.t@hotmail.com>
2020-04-23 01:06:51 +0400

    update styling

--- README.md
+++ README.md
@@ -2,13 +2,14 @@
 
   
 
-# react-native-guide-mark
-
+# React Native Guide Mark
 This React Native module is for guiding the first time user throughout the app
+
+![guide in action](https://lh3.googleusercontent.com/wJjof3378iGsU6EWio3MoYvcqTnANL3Cd-WT5n3ntb5TZA4rhduzGUb2O3aex479q_pMwX8uNAmj57YaBfucZz3XXcAqTyNOacAQNF7h4_0yVsrmfwZw_QyiQYcdDcgmGwCDaA2cNxM0WVkZv_v6LCW2s0zozgN3nhcLR5qE5cRP3DyjjhuPZgUD8y--aRl4reSqfED3CSiQ2Mn-BczTmlU62EDERCMCagoNmmfMth_25p3LsSk5oznd9KJZQb7SUdaTyx0IuZxvX7UD6Q0PMRRSRCcwKgUf8wvwW8h3-WhrzqwFWJnzW3dcapJp2W0RHiqwMpUNA_Xjld0v7bFBvTXryjkFkGWccsXBvazXO1EbEry9hv8VA4-GLkB8uQHfcp2myW2Pnfth_nX0635cX0M8CUXLIbmelPcm4XtnwLwhhc_ZG_nDIuDk4J8ucpq3v3eFHnr6w8J-3UxNdwob616dbT7iNizhB2-87rXTM8cXej8CfSUFjxjedteZwkMgxON3nzSlEkDP3I-7F_TAFT_6yg4Wz2IVkrCVIjqaxlAGNdSLAlpUgyonMpjQ5dxzSl3_M9k0whOcdDqE3nRGp8T3TnI4ERALRRZSvchvWPxywpcMAFw4ZzVkIH2RDV7IHFIUZXEaC54IvOmJMcwnPtksgBF2wwdNN7ZWMfL6JjhW0ZuZQfmi9CT5IM2JZUwycioVEhuvpt9bIhRSkkdLWouBqkV0OUQFJQp8idCTjqe4jBlAbj13gg=w356-h770-no)
   
 ## Installation
-**Yarn**: `yarn add   react-native-guide-mark`
-**NPM**: `npm install react-native-guide-mark`
+**yarn**: `yarn add   react-native-guide-mark`, 
+**npm**: `npm install react-native-guide-mark`
 
 ## Example
 1. import react-native-guide-mark:
@@ -33,6 +34,13 @@ const App = () =>{
 }
 ```
 
+```json
+{
+	fontSize: 34, 
+	fontWeight: "bold",
+	color: 'white'
+}
+```
 ## Props
   
 |  Name |  Type  | Default  |     Description  |
@@ -52,4 +60,7 @@ const App = () =>{
 |`maskBgColor`|String|"rgba(0,0,0,0.75)"|Mask color. |
 |`buttonElm`|React Component|null|Custom button component |
 
-***Author:*** lakshmeesha T
+#### Next up
+ - [ ] Provide ref to element
+ - [ ] Wizard
+ - [ ] Custom hook (if its worthy of doing!)
--- src/GuideMark.js
+++ src/GuideMark.js
@@ -1,4 +1,4 @@
-import React,{PureComponent} from 'react';
+import React from 'react';
 import {
     View,
     StyleSheet,
@@ -9,129 +9,128 @@ import {
 } from 'react-native';
 import RcButtonBright from './RcButtonBright'
 import Modal from 'react-native-modal';
-// import PropTypes from "prop-types";
+import PropTypes from "prop-types";
 const { width:WIDTH, height:HEIGHT } = Dimensions.get('screen');
 
 const MARK_DIMENSION = 150;
 const MARK_IMAGE = require('../assets/coach-mark.png');
 const MASK_BG = "rgba(0,0,0,0.75)";
-
-class GuideMark extends PureComponent{
-    render() {
-        const {
-            title = null,
-            titleTextStyle = null,
-            descriptionStyle = null,
-            description = null,
-            onButtonPress = null,
-            onMarkPress = null,
-            buttonTitle = null,
-            visible = false,
-            left = 0,
-            top = 0,
-            markSize = MARK_DIMENSION,
-            markImage = MARK_IMAGE,
-            maskBgColor=MASK_BG,
-            buttonElm=null
-        } = this.props;
-
-        //Percent calculation
-        let nLeft = typeof left === "string"?(( (WIDTH - markSize) * (parseFloat(left)/100))):left;
-        let nTop = typeof top === "string"?((HEIGHT - markSize) * (parseFloat(top)/100)):top;
-
-        //Content arrangement
-        let xy = [nTop,nLeft];
-        let contentToLeftEnd = xy[1] < (WIDTH*1/5);
-        let contentToCenter = (xy[1] < ((WIDTH*4/5)-markSize)) && !contentToLeftEnd;
-        let contentOnTop = xy[0]+markSize<HEIGHT-markSize;
-
-        let trainingContainerArranged = {
-            top:contentOnTop?xy[0] + markSize : (xy[0] - markSize) - 30,
-            alignItems:contentToCenter?'center':contentToLeftEnd?"flex-start":"flex-end",
-            left: contentToCenter?
-                (WIDTH-300)/2: !contentToLeftEnd?
-                    WIDTH-315 : 15
-        };
-
-        //MaskStyle
-        let topMask = {
-            flex: 0,
-            width:WIDTH,
-            height:xy[0],
-            backgroundColor:maskBgColor
-        };
-        let leftMask = {
-            flex: 0,
-            width:xy[1],
-            height:markSize,
-            backgroundColor:maskBgColor
-        };
-        let markImageStyle ={
-            width:markSize,
-            height:markSize
-        };
-        let rightMask ={
-            flex: 0,
-            width:WIDTH-markSize-xy[1],
-            height:markSize,
-            backgroundColor:maskBgColor
-        };
-        let bottomMask = {
-            flex: 0,
-            width:WIDTH,
-            height:HEIGHT-markSize-xy[0],
-            backgroundColor:maskBgColor
-        };
-
-        return (
-            <Modal
-                isVisible={visible}
-                deviceHeight={HEIGHT}
-                deviceWidth={WIDTH}
-                hasBackdrop={false}
-                coverScreen={true}
-                animationIn={"fadeIn"}
-                animationOut={"fadeOut"}
-                style={styles.modalStyle} >
-
-                <View style={styles.coachContainer} >
-                    <View style={topMask} />
-                    <View style={styles.markRow} >
-                        <View style={leftMask} />
-                        <TouchableWithoutFeedback onPress={onMarkPress}>
-                            <Image   source={markImage} style={markImageStyle} />
-                        </TouchableWithoutFeedback>
-                        <View style={rightMask} />
-                    </View>
-                    <View style={bottomMask} />
-                </View>
-
-                <View style={[ styles.trainingContainer, trainingContainerArranged ]} >
-
-                    {   title &&
-                        <Text style={[styles.trainingTitle,titleTextStyle]}>
-                            {title}
-                        </Text>
-                    }
-                    {
-                        description &&
-                        <Text style={[styles.trainingDesc,{textAlign: contentToCenter ? 'center' : contentToLeftEnd ? "left" : "right"},descriptionStyle]}>
-                            {description}
-                        </Text>
-                    }
-                    {
-                       buttonElm?buttonElm: onButtonPress &&
-                        <RcButtonBright
-                            title={ buttonTitle||"GOT IT"}
-                            style={{marginTop: 20}}
-                            onPress={onButtonPress}
-                        />
-                    }
-                </View>
-
-            </Modal>
-        )
+const GuideMark =(
+    {
+        title = null,
+        titleTextStyle = null,
+        descriptionStyle = null,
+        description = null,
+        onButtonPress = null,
+        onMarkPress = null,
+        buttonTitle = null,
+        visible = false,
+        left = 0,
+        top = 0,
+        markSize = MARK_DIMENSION,
+        markImage = MARK_IMAGE,
+        maskBgColor=MASK_BG,
+        buttonElm=null
     }
+) =>{
+
+    //Percent calculation
+    const nLeft = typeof left === "string"?(( (WIDTH - markSize) * (parseFloat(left)/100))):left;
+    const nTop = typeof top === "string"?((HEIGHT - markSize) * (parseFloat(top)/100)):top;
+
+    //Content arrangement
+    let xy = [nTop,nLeft];
+    let contentToLeftEnd = xy[1] < (WIDTH*1/5);
+    let contentToCenter = (xy[1] < ((WIDTH*4/5)-markSize)) && !contentToLeftEnd;
+    let contentOnTop = xy[0]+markSize<HEIGHT-markSize;
+
+    let trainingContainerArranged = {
+        top:contentOnTop?xy[0] + markSize : (xy[0] - markSize) - 30,
+        alignItems:contentToCenter?'center':contentToLeftEnd?"flex-start":"flex-end",
+        left: contentToCenter?
+            (WIDTH-300)/2: !contentToLeftEnd?
+                WIDTH-315 : 15
+    };
+
+    //MaskStyle
+    let topMask = {
+        flex: 0,
+        width:WIDTH,
+        height:xy[0],
+        backgroundColor:maskBgColor
+    };
+    let leftMask = {
+        flex: 0,
+        width:xy[1],
+        height:markSize,
+        backgroundColor:maskBgColor
+    };
+    let markImageStyle ={
+        width:markSize,
+        height:markSize
+    };
+    let rightMask ={
+        flex: 0,
+        width:WIDTH-markSize-xy[1],
+        height:markSize,
+        backgroundColor:maskBgColor
+    };
+    let bottomMask = {
+        flex: 0,
+        width:WIDTH,
+        height:HEIGHT-markSize-xy[0],
+        backgroundColor:maskBgColor
+    };
+
+    return (
+        <Modal
+            isVisible={visible}
+            deviceHeight={HEIGHT}
+            deviceWidth={WIDTH}
+            hasBackdrop={false}
+            coverScreen={true}
+            animationIn={"fadeIn"}
+            animationOut={"fadeOut"}
+            style={styles.modalStyle} >
+
+            <View style={styles.coachContainer} >
+                <View style={topMask} />
+                <View style={styles.markRow} >
+                    <View style={leftMask} />
+                    <TouchableWithoutFeedback onPress={onMarkPress}>
+                        <Image   source={markImage} style={markImageStyle} />
+                    </TouchableWithoutFeedback>
+                    <View style={rightMask} />
+                </View>
+                <View style={bottomMask} />
+            </View>
+
+            <View style={[ styles.trainingContainer, trainingContainerArranged ]} >
+
+                {   title &&
+                    <Text style={[styles.trainingTitle,titleTextStyle]}>
+                        {title}
+                    </Text>
+                }
+                {
+                    description &&
+                    <Text style={[styles.trainingDesc,{textAlign: contentToCenter ? 'center' : contentToLeftEnd ? "left" : "right"},descriptionStyle]}>
+                        {description}
+                    </Text>
+                }
+                {
+                   buttonElm?buttonElm: onButtonPress &&
+                    <RcButtonBright
+                        title={ buttonTitle||"GOT IT"}
+                        style={{marginTop: 20}}
+                        onPress={onButtonPress}
+                    />
+                }
+            </View>
+
+        </Modal>
+    )
+
 }
 
 GuideMark.defaultProps = {
@@ -147,15 +146,18 @@ GuideMark.defaultProps = {
     markImage :MARK_IMAGE
 }
 
-// GuideMark.prototype = {
-//     onButtonPress : PropTypes.func,
-//     onMarkPress : PropTypes.func,
-//     visible : PropTypes.bool.isRequired,
-//     markSize : PropTypes.number,
-//     markImage : PropTypes.object,
-//     left : PropTypes.oneOfType([PropTypes.string,PropTypes.number]).isRequired,
-//     top : PropTypes.oneOfType([PropTypes.string,PropTypes.number]).isRequired
-// }
+GuideMark.prototype = {
+    title: PropTypes.string,
+    description: PropTypes.string,
+    buttonTitle: PropTypes.string,
+    onButtonPress : PropTypes.func,
+    onMarkPress : PropTypes.func,
+    visible : PropTypes.bool.isRequired,
+    markSize : PropTypes.number,
+    markImage : PropTypes.object,
+    left : PropTypes.oneOfType([PropTypes.string,PropTypes.number]).isRequired,
+    top : PropTypes.oneOfType([PropTypes.string,PropTypes.number]).isRequired
+}
 
 export default GuideMark;
 
@@ -184,7 +186,7 @@ const styles = StyleSheet.create({
         color: 'white',
     },
     trainingDesc:{
-        fontSize: 16,
+        fontSize:16,
         color: 'white',
         fontWeight:"300",
     }
